9dd9e77cc4e0901cf2bd70db32adf95cb5c57ca7
jaydeepmakwana <jaydeep@analogcloudtech.com>
2025-05-21 15:09:42 +0530

    init

--- lib/utils/pageLifecycle.dart
+++ lib/utils/pageLifecycle.dart
@@ -52,3 +52,20 @@ class AppLifecycleHandler with WidgetsBindingObserver {
     _inactiveTimer = null;
   }
 }
+
+
+class Debouncer {
+  final Duration delay;
+  Timer? _timer;
+
+  Debouncer({required this.delay});
+
+  void run(VoidCallback action) {
+    _timer?.cancel();
+    _timer = Timer(delay, action);
+  }
+
+  void dispose() {
+    _timer?.cancel();
+  }
+}
